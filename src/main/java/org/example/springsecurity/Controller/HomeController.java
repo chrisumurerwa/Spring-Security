0a4b8978aa195ec337f9b8df648af3f5ef3893c6
chrisumurerwa <chrisumurerwa1@gmail.com>
2025-11-04 22:33:03 +0200

    Added logic of spring security

---
+++ src/main/java/org/example/springsecurity/Controller/HomeController.java
@@ -0,0 +1,18 @@
+package org.example.springsecurity.Controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "home"; // templates/home.html
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin"; // templates/admin.html
+    }
+}
